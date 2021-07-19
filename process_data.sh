1db06efaea2b046f6e1716c622eeb8dd7bf0de0e
YudongGuo <1025629446@qq.com>
2021-07-19 11:10:52 +0800

    Update process_data.sh

--- process_data.sh
+++ process_data.sh
@@ -1,8 +1,9 @@
 python data_util/process_data.py --id=$1 --step=0 &
 python data_util/process_data.py --id=$1 --step=1
-python data_util/process_data.py --id=$1 --step=2 &
+python data_util/process_data.py --id=$1 --step=2
+python data_util/process_data.py --id=$1 --step=6 &
 python data_util/process_data.py --id=$1 --step=3
 python data_util/process_data.py --id=$1 --step=4
 python data_util/process_data.py --id=$1 --step=5
 wait
-python data_util/process_data.py --id=$1 --step=6
+python data_util/process_data.py --id=$1 --step=7
